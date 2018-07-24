94e6c3cb924d87605a3978135584ebb230d2cc74
tganem <tganem@us.ibm.com>
2018-07-23 23:42:56 -0700

    fix: use token's KeyID to find public key to verify with

--- pkg/token/decode.go
+++ pkg/token/decode.go
@@ -3,6 +3,7 @@ package token
 import (
 	"crypto/rsa"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,18 +14,21 @@ import (
 )
 
 // JwkSetGet will call the url provided JWT_ISSUER and retreive a JWK Set.
-func JwkSetGet(issuer string) ([]jose.JSONWebKey, error) {
+func JwkSetGet(issuer string) (jose.JSONWebKeySet, error) {
+	keyset := jose.JSONWebKeySet{}
 	resp, err := http.Get(issuer)
 	if err != nil {
-		return nil, err
+		return keyset, err
 	}
 	defer resp.Body.Close()
-	log.Info("Retreiving Keyset from", issuer)
-	keyset := jose.JSONWebKeySet{}
 	if err := json.NewDecoder(resp.Body).Decode(&keyset); err != nil && err != io.EOF {
-		return nil, err
+		return keyset, err
 	}
-	return keyset.Keys, nil
+	log.WithFields(log.Fields{
+		"keyset": keyset,
+		"issuer": issuer,
+	}).Info("Retreiving Keyset")
+	return keyset, nil
 }
 
 // Decode the raw token and validate it with a JWK Set.
@@ -34,16 +38,14 @@ func Decode(jwtoken string, issuer string) (jwt.Claims, error) {
 	if err != nil {
 		return claims, fmt.Errorf("Could not read jwt")
 	}
-
-	jwks, err := JwkSetGet(issuer)
-	for _, jwk := range jwks {
-		key := jwk.Key.(*rsa.PublicKey)
-		if err := token.Claims(key, &claims); err == nil {
-			return claims, nil
-		}
+	keyid := token.Headers[0].KeyID
+	jwkset, err := JwkSetGet(issuer)
+	jwk := jwkset.Key(keyid)
+	if len(jwk) == 0 {
+		return claims, errors.New("Can not find token's key id in jwk set")
 	}
 
-	if err != nil {
+	if err := token.Claims(jwk[0].Key.(*rsa.PublicKey), &claims); err != nil {
 		return claims, err
 	}
 
