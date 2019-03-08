8b34b3021780ce1dac72a262c63b90911e0b6add
Tom Ganem <tganem@us.ibm.com>
2019-03-08 05:39:53 +0000

    chore(release): 0.3.0
    [ci skip]

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	// Version should correspond to a git tag
-	Version = "0.2.0"
+	Version = "0.3.0"
 	// ListenPortStr saves the value extracted from the LISTEN_PORT env var
 	ListenPortStr string
 	// ListenPort saves LISTEN_PORT as an integer
